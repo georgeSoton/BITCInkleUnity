437c70ca39af737613cb544a720a7c70d49f5261
George <gelliotthunter@gmail.com>
2020-08-01 16:47:06 +0100

    Updates octo.png. Adds StoryManager.cs

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField]
+    TextAsset StoryJson;
+    public Story story;
+    public StoryManager()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        story = new Story(StoryJson.text);
+    }
+
+    public Color linecolour
+    {
+        get
+        {
+            story.tag
+        }
+    }
+
+    string ProcessTag(string input, string tagname)
+    {
+        if input.StartsWith(tagname + ":"){
+            return input.Substring(tagname.Length).Trim()
+        }
+    }
+}
