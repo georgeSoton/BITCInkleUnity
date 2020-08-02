b4c2816d6f37ae1aa6bbbd1b150243bcb06b10b6
JamieOutram <jamie.outram@btinternet.com>
2020-08-02 14:18:02 +0100

    add transition image prefab + transitionHandler script

---
+++ Assets/TransitionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionHandler : MonoBehaviour
+{
+
+    public Animator transitionAnimator; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Invoke("FadeOut", 2f);
+        Invoke("FadeIn", 4f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn()
+    {
+        transitionAnimator.SetBool("FadeToBlack", false);
+    }
+
+    public void FadeOut()
+    {
+        transitionAnimator.SetBool("FadeToBlack", true);
+    }
+}
