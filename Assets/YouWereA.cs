b37d479b1ba6e0f962ed3a8a0d737b4439edaaf9
George <gelliotthunter@gmail.com>
2020-08-05 21:59:11 +0100

    Fixes endings

--- Assets/YouWereA.cs
+++ Assets/YouWereA.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,8 +15,24 @@ public class YouWereA : MonoBehaviour
         if (turtletype == "") { turtletype = "ordinary"; }
 
         var article = "a";
-        if ("aeiou".Contains(turtletype[0].ToString())) { article = "an";}
+        if ("aeiou".Contains(turtletype[0].ToString().ToLower())) { article = "an";}
 
-        GetComponent<Text>().text = string.Format("Thank you for playing our game.\n You were {0} {1} turtle.", article, turtletype);
+        string endtext;
+        if (turtletype == "lonely")
+        {
+            endtext = "Thank you for playing our game. \n\n You were a lonely turtle. \n\n If you could only try again, perhaps you could save the other captives...";
+        }
+        else
+        {
+            string[] flags = { "convinced_goby", "convinced_mantis", "convinced_young_fish", "convinced_old_fish" };
+            if (flags.All(x=> (int)sm.story.variablesState[x]==1))
+            {
+                endtext = string.Format("Thank you for playing our game.\n\n You were {0} {1} turtle. \n\n You saved everyone you could. Well done!", article, turtletype);
+            } else
+            {
+                endtext = string.Format("Thank you for playing our game.\n\n You were {0} {1} turtle. \n\n You rescued other captives, but could you have convinced more if you had another chance?", article, turtletype);
+            }
+        }
+        GetComponent<Text>().text = endtext;
     }
 }
